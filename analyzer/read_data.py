b165c92f1df23c57af67c652e1db646cf40b90e6
odanori <odanori1980@gmail.com>
2023-11-08 23:07:12 +0900

    feat: データ読み出し機能の追加
    元号、月、地域グループ番号ごとに読み出しデータを整理

--- analyzer/read_data.py
+++ analyzer/read_data.py
@@ -1,3 +1,4 @@
+import re
 import zipfile
 from pathlib import Path
 from typing import List, NamedTuple
@@ -8,6 +9,13 @@ PATH = './data'
 
 
 class InfoAndData(NamedTuple):
+    """csvデータの元号、月、グループ番号、読み出しデータを格納するクラス
+
+    era: 元号
+    month: 月
+    group: 地域グループ番号
+    data: 読み出しデータ
+    """
     era: int
     month: int
     group: int
@@ -40,18 +48,43 @@ def select_zip_file(data_path: Path, target: str) -> Path:
     return target_zip_path
 
 
-def get_csv_filename(target_zip_path: Path) -> List[str]:
-    """TODO: 編集中
+def get_info_and_read_data(filename: str, zip_file: zipfile.ZipFile) -> InfoAndData:
+    """zipファイル中の1つのcsvから、元号、月、地域グループ番号を抽出
+       データ読み込みを行い、NamedTupleクラスにまとめる
 
     Args:
-        target_zip_path (Path): _description_
+        filename (str): csvファイル名(zipfile_name/csvfile_nameの形式)
+        zip_file (zipfile.ZipFile): zipfileモジュールで読み出しモードとなっているzipファイルのオブジェクト
 
     Returns:
-        List[str]: _description_
+        InfoAndData: era->元号、month->月、group->地域グループ番号、data->読み出しデータ(pd.DataFrame)で格納されたNamedTupleクラス
+    """
+    data_info = re.split(r'(od|\.)', filename)[2]
+    group = data_info[-1]
+    month = int(data_info[2:-1])
+    if month > 12:
+        month = int(str(month)[1])
+    if month < 4:
+        era = int(data_info[:2]) - 1
+    else:
+        era = int(data_info[:2])
+    data = pd.read_csv(zip_file.open(filename), encoding='shift-jis')
+    return InfoAndData(era, month, group, data)
+
+
+def read_csv_file(target_zip_path: Path) -> List[InfoAndData]:
+    """zipファイルからcsvファイルを読み出し、元号、月、地域グループ番号ごとにリストにまとめる
+
+    Args:
+        target_zip_path (Path): 調査対象のzipファイルパス
+
+    Returns:
+        List[InfoAndData]: 元号、月、地域グループ番号ごとに整理された読み出しデータのリスト
     """
     with zipfile.ZipFile(target_zip_path, 'r') as zip_file:
-        n = [filename for filename in zip_file.namelist()]
-        print(n)
+        data_l = sorted([get_info_and_read_data(filename, zip_file)
+                         for filename in zip_file.namelist()])
+    return data_l
 
 
 def read_zip_file(target):
@@ -62,4 +95,5 @@ def read_zip_file(target):
     """
     data_path = make_data_path()
     target_zip_path = select_zip_file(data_path, target)
-    get_csv_filename(target_zip_path)
+    data_l = read_csv_file(target_zip_path)
+    print(data_l)
