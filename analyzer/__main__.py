cd4de4858ce600edaa3dbaf6347be875fda0c041
odanori <odanori1980@gmail.com>
2023-11-09 17:26:41 +0900

    refactor: データに元号と地域グループ番号を追加
    それに従いdocstring修正

--- analyzer/__main__.py
+++ analyzer/__main__.py
@@ -46,7 +46,7 @@ def run():
 
     preprocess = Preprocessing(config)
     processed_data_l = [preprocess.data_preprocess(
-        info_and_data.data) for info_and_data in info_and_data_l]
+        info_and_data.data, info_and_data.era, info_and_data.group) for info_and_data in info_and_data_l]
     print(processed_data_l)
 
 
--- analyzer/preprocessing.py
+++ analyzer/preprocessing.py
@@ -13,12 +13,52 @@ class Preprocessing():
         self.config = config
         self.out_of_range_log_path = Path('./out_of_range')
 
+    def delete_nan_row(self, data: pd.DataFrame) -> pd.DataFrame:
+        """データ行のすべてがnanのレコードを削除する
+           例:日付~栄養を示すデータがすべてnanの場合、削除
+            A日付  献立名 材料名 分量（g）  エネルギー（kcal）  たんぱく質（g）  脂質（g）  ナトリウム（mg）
+            nan    nan    nan    nan        nan                 nan            nan         nan
+        Args:
+            data (pd.DataFrame): csvデータ
+                        例:A日付  献立名   材料名   分量（g）  エネルギー（kcal）  たんぱく質（g）  脂質（g）  ナトリウム（mg）
+                        2023/11/1   黒パン  黒パン    50       236                7.1            3.8         358
+                        2023/11/1   牛乳    牛乳     206       126                6.8            7.8         84
+                        nan         nan     nan      nan      nan                nan            nan         nan
+
+        Returns:
+            pd.DataFrame: データ行すべてがnanのレコードを削除したデータ
+                        例:A日付  献立名   材料名   分量（g）  エネルギー（kcal）  たんぱく質（g）  脂質（g）  ナトリウム（mg）
+                        2023/11/1   黒パン  黒パン    50       236                7.1            3.8         358
+                        2023/11/1   牛乳    牛乳     206       126                6.8            7.8         84
+        """
+        data.dropna(how='all', axis=0, inplace=True)
+        return data
+
+    def add_era_and_group(self, data: pd.DataFrame, era: int, group: int) -> pd.DataFrame:
+        """データに元号と地域グループ番号を追加
+
+        Args:
+            data (pd.DataFrame): delete_nan_rowで処理されたデータ
+                           例:A日付  献立名   材料名   分量（g）  ...
+                         2023/11/1   黒パン  黒パン    50
+            era (int): 元号
+            group (int): 地域グループ番号
+
+        Returns:
+            pd.DataFrame: 元号、地域グループ番号が追加されたデータ
+                        例:era group    A日付  献立名   材料名   分量（g）  ...
+                            5    a  2023/11/1   黒パン  黒パン    50
+        """
+        data.insert(0, 'era', era)
+        data.insert(1, 'group', group)
+        return data
+
     def separate_column_name(self, data: pd.DataFrame) -> pd.DataFrame:
         """データごとに同じ意味で異なるカラム名を統一する
            カラム名で共通する部分をjsonファイルで指定し、その部分だけで統一
 
         Args:
-            data (pd.DataFrame): csvから読み出したデータ
+            data (pd.DataFrame): csvデータ
 
         Raises:
             Exception: 変更対象のカラムが複数ある場合は例外処理
@@ -30,8 +70,8 @@ class Preprocessing():
                               A日付, 献立, 材料名,...
                               地域グループ番号Bのデータのカラム
                               B日付, 献立, 材料名,...
-                              →日付カラムの先頭には地域グループ番号がつくが、どのデータの日付も同じ意味のため、
-                              グループ番号と日付を分離し、日付カラムの名称を統一する
+                              変更後
+                              日付, 献立, 材料名,...
         """
         # FIXME: 現状、日付のみの対応
         columns = data.columns.values
@@ -41,10 +81,8 @@ class Preprocessing():
             raise Exception(
                 f'変更対象:{self.config.rename_col}が1カラムより多く存在します。確認ください:\n{data}')
         rename_idx = col_idx_l[0]
-        group = columns[rename_idx].replace(self.config.rename_col, '')
         rename_col = {columns[rename_idx]: self.config.rename_col}
         data.rename(columns=rename_col, inplace=True)
-        data.insert(0, 'group', group.lower())
         return data
 
     def delete_unname_columns(self, data: pd.DataFrame) -> pd.DataFrame:
@@ -52,15 +90,19 @@ class Preprocessing():
            カラム名がないにもかかわらず、何らかのデータがあった場合は
            out_of_range_logファイルに記載
            例: out_of_range_logファイルの中身
-               group     日付       献立名     材料名 分量（g）  エネルギー（kcal）  たんぱく質（g）  脂質（g）  ナトリウム（mg）  Unnamed: 8 Unnamed: 9
-                  c  2022/9/6  あじのこはく揚げ   油    35         21.0              0.0            2.4            0.0          NaN       2022/6/16
+               era group     日付       献立名     材料名 分量（g）  エネルギー（kcal）  たんぱく質（g）  脂質（g）  ナトリウム（mg）  Unnamed: 8 Unnamed: 9
+                 4   c  2022/9/6  あじのこはく揚げ   油    35         21.0              0.0            2.4            0.0          NaN       2022/6/16
                   →データがないはずのUnnamed:9カラムに日付らしきものがある
 
         Args:
-            data (pd.DataFrame): csvから読み出したデータ
+            data (pd.DataFrame): csvデータ
+                      例 era group     日付       献立名     材料名 分量（g）  エネルギー（kcal）  たんぱく質（g）  脂質（g）  ナトリウム（mg）  Unnamed: 8 Unnamed: 9
+                           4   c  2022/11/1      麦ごはん    米     70         239               4.3             0.6            1.0          NaN        nan
 
         Returns:
-            pd.DataFrame: 引数のデータからUnnamedカラムを削除処理されたもの
+            pd.DataFrame: 引数のデータからUnnamedカラムが削除処理されたもの
+                    例 era group     日付       献立名     材料名 分量（g）  エネルギー（kcal）  たんぱく質（g）  脂質（g）  ナトリウム（mg）
+                           4   c  2022/11/1      麦ごはん    米     70         239               4.3             0.6            1.0
         """
         columns = data.columns.values
         del_col_l = [col for col in columns if col.startswith('Unnamed')]
@@ -75,30 +117,26 @@ class Preprocessing():
             data.drop(columns=del_col_l, inplace=True)
         return data
 
-    def delete_nan_row(self, data: pd.DataFrame) -> pd.DataFrame:
-        """すべてのデータがnanの行を削除する
-           例:日付~栄養を示すデータがすべてnanの場合、削除
-              group   日付  献立名 材料名 分量（g）  エネルギー（kcal）  たんぱく質（g）  脂質（g）  ナトリウム（mg）
-
+    def data_formatting(self, data: pd.DataFrame, era: int, group: int) -> pd.DataFrame:
+        """入力データ範囲外のエラーデータを削除する
+           元号や地域グループ番号を追加する
 
         Args:
-            data (pd.DataFrame): _description_
+            data (pd.DataFrame): csvデータ
+            era (int): 元号
+            group (int): 地域グループ番号
 
         Returns:
-            pd.DataFrame: _description_
+            pd.DataFrame: 整形されたデータ
         """
-        data = data.copy()
-        data.dropna(how='all', axis=0, inplace=True)
-        return data
-
-    def data_formatting(self, data: pd.DataFrame) -> pd.DataFrame:
         formatting_data = data.copy()
         formatting_data = self.delete_nan_row(formatting_data)
+        formatting_data = self.add_era_and_group(formatting_data, era, group)
         formatting_data = self.separate_column_name(formatting_data)
         formatting_data = self.delete_unname_columns(formatting_data)
         del data
         return formatting_data
 
-    def data_preprocess(self, data: pd.DataFrame) -> pd.DataFrame:
-        formatting_data = self.data_formatting(data)
+    def data_preprocess(self, data: pd.DataFrame, era: int, group: int) -> pd.DataFrame:
+        formatting_data = self.data_formatting(data, era, group)
         return formatting_data
