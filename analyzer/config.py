a353561a102f58f0d267e2e711b373336a2582a5
odanori <odanori1980@gmail.com>
2023-11-10 20:52:03 +0900

    refactor: nan_colの削除
    材料名ごとに欠損値の対応を変える予定なので、
    材料名カラムを固定して欠損値がある材料名を抽出するようにする

--- analyzer/config.py
+++ analyzer/config.py
@@ -4,10 +4,10 @@ from typing import Dict
 class Config():
 
     def __init__(self, config_json: Dict) -> None:
-        """_summary_
+        """前処理の設定項目config.jsonの値を変数に代入する
 
         Args:
-            config_json (Dict): _description_
+            config_json (Dict): config.jsonを読み込んだ内容
         """
         self.config = config_json
         formating = self.config['formatting']
@@ -17,7 +17,6 @@ class Config():
 
         self.rename_col = formating['rename_col']
 
-        self.nan_col = manipulating['nan_col']
         self.nan_name = manipulating['nan_name']
         self.str_num_col = manipulating['str_num_col']
         self.datetime_col = manipulating['datetime_col']
