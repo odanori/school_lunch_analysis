3b3dff8fc3ca2201f9a4d84e1ae39398a273ce55
odanori <odanori1980@gmail.com>
2023-12-07 01:07:38 +0900

    chore: DB接続情報例の更新

--- v2_analyzer/settings/config.py
+++ v2_analyzer/settings/config.py
@@ -2,6 +2,6 @@
 
 class Config:
     # DB接続情報
-    POSTGRES_URI = 'postgresql://{user}:{pass}@{host}:{port}/lunch_menu'
+    POSTGRES_URI = 'postgresql://{user}:{pass}@{host}:{port}/{db_name}'
     POSTGRES_FILENAME_TABLE = 'filenames'
     POSTGRES_BASE_TABLE_NAME = 'menu'
