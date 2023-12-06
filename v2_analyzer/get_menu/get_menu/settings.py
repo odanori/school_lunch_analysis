3c60c0c222cbc698bac11b170bbb5ea17e577b3c
odanori <odanori1980@gmail.com>
2023-12-06 13:43:00 +0900

    refactor: settingsディレクトリを作成しconfig.pyを移動

--- v2_analyzer/get_menu/get_menu/settings.py
+++ v2_analyzer/get_menu/get_menu/settings.py
@@ -7,7 +7,7 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-from v2_analyzer.config import Config
+from v2_analyzer.settings.config import Config
 
 BOT_NAME = "get_menu"
 
