1c5d192de0c7c8820f3c89e09e186f5882208e72
odanori <odanori1980@gmail.com>
2024-01-10 10:51:45 +0900

    chore: db接続情報を正しく記載
    手元で確認用。gitには上げない

--- v2_analyzer/settings/config.py
+++ v2_analyzer/settings/config.py
@@ -2,6 +2,6 @@
 
 class Config:
     # DB接続情報
-    POSTGRES_URI = 'postgresql://{user}:{pass}@{host}:{port}/{db_name}'
+    POSTGRES_URI = 'postgresql://postgres:postgres@localhost:5432/lunch_menu'
     POSTGRES_FILENAME_TABLE = 'filenames'
     POSTGRES_BASE_TABLE_NAME = 'menu'
